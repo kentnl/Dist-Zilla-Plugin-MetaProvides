337d808668e89c2a43f7f5045b8eab53cf7a00ec
Kent Fredric <kentfredric@gmail.com>
2009-06-02 13:48:31 +1200

    Provider Role Documentation

--- lib/Dist/Zilla/Role/MetaProvider/Provider.pm
+++ lib/Dist/Zilla/Role/MetaProvider/Provider.pm
@@ -1,5 +1,7 @@
 package Dist::Zilla::Role::MetaProvider::Provider;
 
+# ABSTRACT: A Role for Metadata providers specific to the 'provider' key.
+
 # $Id:$
 use strict;
 use warnings;
@@ -7,9 +9,26 @@ use Moose::Role;
 
 use namespace::autoclean;
 
+=head1 PERFORMS ROLES
+
+L<Dist::Zilla::Role::MetaProvider>
+
+=cut
+
 with 'Dist::Zilla::Role::MetaProvider';
 
+=head1 REQUIRED METHODS FOR PERFORMING ROLES
+
+=head2 provides
+
+Must return an array full of L<Dist::Zilla::MetaProvider::ProvideRecord>
+instances.
+
+=cut
+
 requires 'provides';
+
+=head1 ATTRIBUTES / PARAMETERS
 
 =head2 inherit_version
 
@@ -23,7 +42,7 @@ the authority, and all versions discovered in packages are ignored.
 Setting this value to '0' makes the version defined in the discovered class
 the authority, and it is copied to the provides metadata.
 
-This option also controls data in the extra_files list.
+( To use this feature in a performing class, see L</_resolve_version> )
 
 =cut
 
@@ -45,6 +64,8 @@ which is permissible.
 Setting this value to "1" ( the default ) results in dist.ini's version being used
 instead.
 
+( To use this feature in a performing class, see L</_resolve_version> )
+
 =cut
 
 has inherit_missing => (
@@ -55,6 +76,26 @@ has inherit_missing => (
     'How to behave when we are trusting modules to have versions and one is'
     . ' missing one',
 );
+
+=head1 PRIVATE METHODS
+
+=head2 _resolve_version
+
+This is a utility method to make performing classes life easier in adhering to
+user requirements.
+
+    my $params  = {
+        file => $somefile ,
+        $self->_resolve_version( $version );
+    }
+
+is the suggested used.
+
+Returns either an empty list, or a list with C<('version', $version )>;
+
+This is so C<{ version => undef }> does not occur in the YAML.
+
+=cut
 
 sub _resolve_version {
   my $self    = shift;
@@ -70,11 +111,20 @@ sub _resolve_version {
   return ( 'version', $version );
 }
 
+=head1 PUBLIC METHODS
+
+=head2 metadata
+
+Fullfills the requirement of L<Dist::Zilla::Role::MetaProvider> by processing
+results returned from C<$self->provides>.
+
+=cut
+
 sub metadata {
   my ($self) = @_;
   my $discover = {};
-  for ( $self->provides ){
-      $_->copy_into( $discover );
+  for ( $self->provides ) {
+    $_->copy_into($discover);
   }
   return { provides => $discover };
 }
