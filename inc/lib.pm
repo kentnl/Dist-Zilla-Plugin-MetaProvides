25941933d7009cdb80f21092109042d90eef4053
Kent Fredric <kentfredric@gmail.com>
2010-05-22 18:59:22 +1200

    Replaced most of dist.ini with @KENTNL and use =inc::lib
      Replaced most of dist.ini with @KENTNL
        ( Yeilding more accurate t/ depends )
      Removed =inc::Dist style loopback in favour
        of the more reliable and more generic
        =inc::lib

---
+++ inc/lib.pm
@@ -0,0 +1,19 @@
+package inc::lib;
+
+# $Id:$
+use strict;
+use warnings;
+use Moose;
+use Cwd;
+my $lib;
+BEGIN { $lib = cwd . "/lib/"; }
+use Carp;
+use lib "$lib";
+Carp::carp("[inc::lib] $lib added to \@INC");
+
+sub log_debug   { 1; }
+sub plugin_name { 'inc::lib' }
+sub dump_config { }
+
+1;
+
