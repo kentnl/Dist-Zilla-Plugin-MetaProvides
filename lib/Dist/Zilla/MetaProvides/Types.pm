06073e9dd72f0349c59027d757ad20da6c94fbe2
Kent Fredric <kentfredric@gmail.com>
2013-11-21 22:09:02 +1300

    Build results of c8c5935 (on master)

--- lib/Dist/Zilla/MetaProvides/Types.pm
+++ lib/Dist/Zilla/MetaProvides/Types.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::MetaProvides::Types::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::MetaProvides::Types::VERSION = '1.15000001';
+  $Dist::Zilla::MetaProvides::Types::VERSION = '1.15000100';
 }
 
 # ABSTRACT: Utility Types for the MetaProvides Plugin
@@ -34,7 +34,7 @@ Dist::Zilla::MetaProvides::Types - Utility Types for the MetaProvides Plugin
 
 =head1 VERSION
 
-version 1.15000001
+version 1.15000100
 
 =begin MetaPOD::JSON v1.1.0
 
