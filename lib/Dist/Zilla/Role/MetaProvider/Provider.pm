3909f2c4b11a163f2e8e5a822906b20c35a63925
Kent Fredric <kentfredric@gmail.com>
2014-08-19 13:22:04 +1200

    Build results of 593c3c4 (on master)

--- lib/Dist/Zilla/Role/MetaProvider/Provider.pm
+++ lib/Dist/Zilla/Role/MetaProvider/Provider.pm
@@ -13,8 +13,6 @@ our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
 use Moose::Role qw( with requires has around );
 use MooseX::Types::Moose qw( Bool );
-use Readonly qw( Readonly );
-Readonly my $MIN_EMULATE_PHASE_VERSION => 0.01000101;
 use namespace::autoclean;
 
 
