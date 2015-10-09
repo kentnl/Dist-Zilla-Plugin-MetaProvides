0e40bdb3ea217d91c70acf89454e088d68a788ff
Kent Fredric <kentfredric@gmail.com>
2015-10-09 15:37:22 +1300

    Build results of e6d0528 (on master)

--- lib/Dist/Zilla/Plugin/MetaProvides.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides.pm
@@ -219,7 +219,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2014 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
