4003ae2971a5e6205e3d47df84c1f0dfdb270350
Kent Fredric <kentfredric@gmail.com>
2015-10-09 20:03:12 +1300

    v2.001010
     [Dependencies::Stats]
     - Dependencies changed since 2.001002, see misc/*.deps* for details
     - configure: (recommends: ↓1)
     - develop: +8 ↑3 -1 (recommends: ↓1, suggests: ↑2)
     - runtime: (recommends: +1)
     - test: (recommends: ↓2)
    
     [Documentation]
     - Remove confusing quick reference and try to find a better way another day
     - kentnl/Dist-Zilla-Plugin-MetaProvides-Package#4
    
     [Features]
     - Experimentally injecting the version of the role into `dump_config` data.
     - Part of request #3

--- lib/Dist/Zilla/MetaProvides/Types.pm
+++ lib/Dist/Zilla/MetaProvides/Types.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::MetaProvides::Types;
 
-our $VERSION = '2.001003';
+our $VERSION = '2.001011';
 
 # ABSTRACT: Utility Types for the MetaProvides Plugin
 
--- lib/Dist/Zilla/Plugin/MetaProvides.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides;
 
-our $VERSION = '2.001010';
+our $VERSION = '2.001011';
 
 # ABSTRACT: Generating and Populating 'provides' in your META.yml
 
