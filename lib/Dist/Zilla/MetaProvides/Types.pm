0baae0ad03b1fd1927a4b4f6f4463fe6c2043dc3
Kent Fredric <kentfredric@gmail.com>
2016-04-23 06:17:19 +1200

    Build results of bdaa531 (on master)

--- lib/Dist/Zilla/MetaProvides/Types.pm
+++ lib/Dist/Zilla/MetaProvides/Types.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::MetaProvides::Types;
 
-our $VERSION = '2.002000'; # TRIAL
+our $VERSION = '2.002001';
 
 # ABSTRACT: Utility Types for the MetaProvides Plugin
 
@@ -60,7 +60,7 @@ Dist::Zilla::MetaProvides::Types - Utility Types for the MetaProvides Plugin
 
 =head1 VERSION
 
-version 2.002000
+version 2.002001
 
 =head1 SUBTYPES
 
--- lib/Dist/Zilla/Plugin/MetaProvides.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides;
 
-our $VERSION = '2.002000'; # TRIAL
+our $VERSION = '2.002001';
 
 # ABSTRACT: Generating and Populating 'provides' in your META.yml
 
@@ -44,7 +44,7 @@ Dist::Zilla::Plugin::MetaProvides - Generating and Populating 'provides' in your
 
 =head1 VERSION
 
-version 2.002000
+version 2.002001
 
 =head1 SYNOPSIS
 
