cfc9db4f184c6f8c7210b0b7e68075ee600fa1ea
Kent Fredric <kentfredric@gmail.com>
2016-06-29 18:37:45 +1200

    Build results of e2696bc (on master)

--- lib/Dist/Zilla/Role/MetaProvider/Provider.pm
+++ lib/Dist/Zilla/Role/MetaProvider/Provider.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Role::MetaProvider::Provider;
 
-our $VERSION = '2.002001'; # TRIAL
+our $VERSION = '2.002002';
 
 # ABSTRACT: A Role for Metadata providers specific to the 'provider' key.
 
@@ -294,7 +294,7 @@ Dist::Zilla::Role::MetaProvider::Provider - A Role for Metadata providers specif
 
 =head1 VERSION
 
-version 2.002001
+version 2.002002
 
 =head1 PUBLIC METHODS
 
